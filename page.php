a2a5ef218a2913885348de1746305fd6b69a80b6
edrisranjbar <edris.qeshm2@gmail.com>
2019-05-18 01:30:05 +0430

    some changes in page layout

--- page.php
+++ page.php
@@ -1,24 +1,7 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package qurandl
- */
-
-get_header();
-?>
-
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
+		<main id="main">
 		<?php
+        get_header();
 		while ( have_posts() ) :
 			the_post();
 
