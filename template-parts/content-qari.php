6e2873eef3208e85b269d742705710f5fb4024d9
edrisranjbar <edris.qeshm2@gmail.com>
2019-07-13 23:19:05 +0430

    Live Search

--- template-parts/content-qari.php
+++ template-parts/content-qari.php
@@ -2,7 +2,13 @@
         <?php
             qurandl_post_thumbnail();
             the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
-            echo "<input id='myInput' onkeyup='myFunction()' placeholder='نام سوره ی مورد نظر را جست و جو کنید...'>";
+        ?>
+    <div class="row">
+        <div class="form-group">
+            <input class="form-control" id="myFunction()" placeholder="نام سوره ی مورد نظر را جست و جو کنید...">
+        </div>
+    </div>
+        <?php
             the_content();
         ?>
     </article><!-- #post-<?php the_ID(); ?> -->
